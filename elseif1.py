68f4bfee55448cb17a78a58ff1e9ae64decd6312
dhurvchudasama27 <dhruvchudasama999@gmail.com>
2023-12-27 10:47:16 +0530

    python

---
+++ elseif1.py
@@ -0,0 +1,212 @@
+male_birth_day = int(input("Entre male birth day "))
+male_birth_month = int(input("Enter male birth month "))
+female_birth_day = int(input("Entre female birth day "))
+female_birth_month = int(input("Enter female birth month "))
+male_sign = ""
+female_sign = ""
+
+if male_birth_month == 3 and male_birth_day >= 21 and male_birth_day <= 31:
+    print("male zodiac sign is aries")
+    male_sign = "aries"
+elif male_birth_month == 4 and male_birth_day <= 19 and male_birth_day > 0:
+    print("male zodiac sign is aries")
+    male_sign = "aries"
+elif male_birth_month == 4 and male_birth_day >= 20 and male_birth_day < 30:
+    print("male zodiac sign is taurus")
+    male_sign = "taurus"
+elif male_birth_month == 5 and male_birth_day <= 20 and male_birth_day > 0:
+    print("male zodiac sign is taurus")
+    male_sign = "taurus"
+elif male_birth_month == 5 and male_birth_day >= 21 and male_birth_day <= 31:
+    print("male zodiac sign is gemini")
+    male_sign = "gemini"
+elif male_birth_month == 6 and male_birth_day <= 20 and male_birth_day > 0:
+    print("male zodiac sign is gemini")
+    male_sign = "gemini"
+elif male_birth_month == 6 and male_birth_day >= 21 and male_birth_day <= 30:
+    print("male zodiac sign is cancer")
+    male_sign = "cancer"
+elif male_birth_month == 7 and male_birth_day <= 22 and male_birth_day > 0:
+    print("male zodiac sign is cancer")
+    male_sign = "cancer"
+elif male_birth_month == 7 and male_birth_day >= 23 and male_birth_day <= 31:
+    print("male zodiac sign is leo")
+    male_sign = "leo"
+elif male_birth_month == 8 and male_birth_day <= 22 and male_birth_day > 0:
+    print("male zodiac sign is leo")
+    male_sign = "leo"
+elif male_birth_month == 8 and male_birth_day >= 23 and male_birth_day <= 31:
+    print("male zodiac sign is virgo")
+    male_sign = "virgo"
+elif male_birth_month == 9 and male_birth_day <= 22 and male_birth_day > 0:
+    print("male zodiac sign is virgo")
+    male_sign = "virgo"
+elif male_birth_month == 9 and male_birth_day >= 23 and male_birth_day <= 30:
+    print("male zodiac sign is libra")
+    male_sign = "libra"
+elif male_birth_month == 10 and male_birth_day <= 22 and male_birth_day > 0:
+    print("male zodiac sign is libra")
+    male_sign = "libra"
+elif male_birth_month == 10 and male_birth_day >= 23 and male_birth_day <= 31:
+    print("male zodiac sign is scorpio")
+    male_sign = "scorpio"
+elif male_birth_month == 11 and male_birth_day <= 21 and male_birth_day > 0:
+    print("male zodiac sign is scorpio")
+    male_sign = "scorpio"
+elif male_birth_month == 11 and male_birth_day >= 22 and male_birth_day <= 30:
+    print("male zodiac sign is sagittarius")
+    male_sign = "sagittarius"
+elif male_birth_month == 12 and male_birth_day <= 21 and male_birth_day > 0:
+    print("male zodiac sign is sagittarius")
+    male_sign = "sagittarius"
+elif male_birth_month == 12 and male_birth_day >= 22 and male_birth_day <= 31:
+    print("male zodiac sign is capricorn")
+    male_sign = "capricorn"
+elif male_birth_month == 1 and male_birth_day <= 19 and male_birth_day > 0:
+    print("male zodiac sign is capricorn")
+    male_sign = "capricorn"
+elif male_birth_month == 1 and male_birth_day >= 20 and male_birth_day <= 31:
+    print("male zodiac sign is aquarius")
+    male_sign = "aquarius"
+elif male_birth_month == 2 and male_birth_day <= 18 and male_birth_day > 0:
+    print("male zodiac sign is aquarius")
+    male_sign = "aquarius"
+elif male_birth_month == 2 and male_birth_day >= 19 and male_birth_day <= 29:
+    print("male zodiac sign is pisces")
+    male_sign = "pisces"
+elif male_birth_month == 3 and male_birth_day <= 20 and male_birth_day > 0:
+    print("male zodiac sign is pisces")
+    male_sign = "pisces"
+
+
+if female_birth_month == 3 and female_birth_day >= 21 and female_birth_day <= 31:
+    print("female zodiac sign is aries")
+    female_sign = "aries"
+elif female_birth_month == 4 and female_birth_day <= 19 and female_birth_day > 0:
+    print("female zodiac sign is aries")
+    female_sign = "aries"
+elif female_birth_month == 4 and female_birth_day >= 20 and female_birth_day < 30:
+    print("female zodiac sign is taurus")
+    female_sign = "taurus"
+elif female_birth_month == 5 and female_birth_day <= 20 and female_birth_day > 0:
+    print("female zodiac sign is taurus")
+    female_sign = "taurus"
+elif female_birth_month == 5 and female_birth_day >= 21 and female_birth_day <= 31:
+    print("female zodiac sign is gemini")
+    female_sign = "gemini"
+elif female_birth_month == 6 and female_birth_day <= 20 and female_birth_day > 0:
+    print("female zodiac sign is gemini")
+    female_sign = "gemini"
+elif female_birth_month == 6 and female_birth_day >= 21 and female_birth_day <= 30:
+    print("female zodiac sign is cancer")
+    female_sign = "cancer"
+elif female_birth_month == 7 and female_birth_day <= 22 and female_birth_day > 0:
+    print("female zodiac sign is cancer")
+    female_sign = "cancer"
+elif female_birth_month == 7 and female_birth_day >= 23 and female_birth_day <= 31:
+    print("female zodiac sign is leo")
+    female_sign = "leo"
+elif female_birth_month == 8 and female_birth_day <= 22 and female_birth_day > 0:
+    print("female zodiac sign is leo")
+    female_sign = "leo"
+elif female_birth_month == 8 and female_birth_day >= 23 and female_birth_day <= 31:
+    print("female zodiac sign is virgo")
+    female_sign = "virgo"
+elif female_birth_month == 9 and female_birth_day <= 22 and female_birth_day > 0:
+    print("female zodiac sign is virgo")
+    female_sign = "virgo"
+elif female_birth_month == 9 and female_birth_day >= 23 and female_birth_day <= 30:
+    print("female zodiac sign is libra")
+    female_sign = "libra"
+elif female_birth_month == 10 and female_birth_day <= 22 and female_birth_day > 0:
+    print("female zodiac sign is libra")
+    female_sign = "libra"
+elif female_birth_month == 10 and female_birth_day >= 23 and female_birth_day <= 31:
+    print("female zodiac sign is scorpio")
+    female_sign = "scorpio"
+elif female_birth_month == 11 and female_birth_day <= 21 and female_birth_day > 0:
+    print("female zodiac sign is scorpio")
+    female_sign = "scorpio"
+elif female_birth_month == 11 and female_birth_day >= 22 and female_birth_day <= 30:
+    print("female zodiac sign is sagittarius")
+    female_sign = "sagittarius"
+elif female_birth_month == 12 and female_birth_day <= 21 and female_birth_day > 0:
+    print("female zodiac sign is sagittarius")
+    female_sign = "sagittarius"
+elif female_birth_month == 12 and female_birth_day >= 22 and female_birth_day <= 31:
+    print("female zodiac sign is capricorn")
+    female_sign = "capricorn"
+elif female_birth_month == 1 and female_birth_day <= 19 and female_birth_day > 0:
+    print("female zodiac sign is capricorn")
+    female_sign = "capricorn"
+elif female_birth_month == 1 and female_birth_day >= 20 and female_birth_day <= 31:
+    print("female zodiac sign is aquarius")
+    female_sign = "aquarius"
+elif female_birth_month == 2 and female_birth_day <= 18 and female_birth_day > 0:
+    print("female zodiac sign is aquarius")
+    female_sign = "aquarius"
+elif female_birth_month == 2 and female_birth_day >= 19 and female_birth_day <= 29:
+    print("female zodiac sign is pisces")
+    female_sign = "pisces"
+elif female_birth_month == 3 and female_birth_day <= 20 and female_birth_day > 0:
+    print("female zodiac sign is pisces")
+    female_sign = "pisces"
+
+
+if male_sign == "aries" or male_sign == "libra":
+    if female_sign == "aries" or female_sign == "gemini" or female_sign == "leo" or female_sign == "libra" or female_sign == "sagittarius" or female_sign == "aquarius":
+        print("Favourable : should be an excellent love match")
+    elif female_sign == "taurus" or female_sign == "virgo" or female_sign == "scorpio" or female_sign == "pisces":
+        print("Less favourable : may be good, but both of you need to continue to work on the relationship")
+    elif female_sign == "cancer" or female_sign == "capricorn":
+        print("critical : can be hard to make this relationship progress smoothly")
+elif male_sign == "taurus" or male_sign == "scorpio":
+    if female_sign == "taurus" or female_sign == "cancer" or female_sign == "virgo" or female_sign == "scorpio" or female_sign == "capricorn" or female_sign == "pisces":
+        print("Favourable : should be an excellent love match")
+    elif female_sign == "aries" or female_sign == "gemini" or female_sign == "libra" or female_sign == "sagittarius":
+        print("Less favourable : may be good, but both of you need to continue to work on the relationship")
+    elif female_sign == "leo" or female_sign == "aquarius":
+        print("critical : can be hard to make this relationship progress smoothly")
+elif male_sign == "gemini" or male_sign == "sagittarius":
+    if female_sign == "aries" or female_sign == "gemini" or female_sign == "leo" or female_sign == "libra" or female_sign == "sagittarius" or female_sign == "aquarius":
+        print("Favourable : should be an excellent love match")
+    elif female_sign == "taurus" or female_sign == "cancer" or female_sign == "scorpio" or female_sign == "capricorn":
+        print("Less favourable : may be good, but both of you need to continue to work on the relationship")
+    elif female_sign == "virgo" or female_sign == "pisces":
+        print("critical : can be hard to make this relationship progress smoothly")
+elif male_sign == "cancer":
+    if female_sign == "taurus" or female_sign == "cancer" or female_sign == "virgo" or female_sign == "scorpio" or female_sign == "capricorn" or female_sign == "pisces":
+        print("Favourable : should be an excellent love match")
+    elif female_sign == "gemini" or female_sign == "leo" or female_sign == "sagittarius" or female_sign == "aquarius":
+        print("Less favourable : may be good, but both of you need to continue to work on the relationship")
+    elif female_sign == "aries" or female_sign == "libra":
+        print("critical : can be hard to make this relationship progress smoothly")
+elif male_sign == "leo":
+    if female_sign == "aries" or female_sign == "gemini" or female_sign == "leo" or female_sign == "libra" or female_sign == "sagittarius" or female_sign == "aquarius":
+        print("Favourable : should be an excellent love match")
+    elif female_sign == "cancer" or female_sign == "virgo" or female_sign == "capricorn":
+        print("Less favourable : may be good, but both of you need to continue to work on the relationship")
+    elif female_sign == "taurus" or female_sign == "scorpio" or female_sign == "pisces":
+        print("critical : can be hard to make this relationship progress smoothly")
+elif male_sign == "virgo" or male_sign == "pisces":
+    if female_sign == "taurus" or female_sign == "cancer" or female_sign == "virgo" or female_sign == "scorpio" or female_sign == "capricorn" or female_sign == "pisces":
+        print("Favourable : should be an excellent love match")
+    elif female_sign == "aries" or female_sign == "leo" or female_sign == "libra" or female_sign == "aquarius":
+        print("Less favourable : may be good, but both of you need to continue to work on the relationship")
+    elif female_sign == "gemini" or female_sign == "sagittarius":
+        print("critical : can be hard to make this relationship progress smoothly")
+elif male_sign == "capricorn":
+    if female_sign == "taurus" or female_sign == "cancer" or female_sign == "virgo" or female_sign == "scorpio" or female_sign == "capricorn" or female_sign == "pisces":
+        print("Favourable : should be an excellent love match")
+    elif female_sign == "aries" or female_sign == "gemini" or female_sign == "leo" or female_sign == "libra" or female_sign == "sagittarius" or female_sign == "aquarius":
+        print("critical : can be hard to make this relationship progress smoothly")
+elif male_sign == "aquarius":
+    if female_sign == "aries" or female_sign == "gemini" or female_sign == "leo" or female_sign == "libra" or female_sign == "sagittarius" or female_sign == "aquarius":
+        print("Favourable : should be an excellent love match")
+    elif female_sign == "cancer" or female_sign == "virgo" or female_sign == "capricorn" or female_sign == "pisces":
+        print("Less favourable : may be good, but both of you need to continue to work on the relationship")
+    elif female_sign == "taurus" or female_sign == "scorpio":
+        print("critical : can be hard to make this relationship progress smoothly")
+
+
+
